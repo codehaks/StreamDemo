236a6958438d75a780716154344b8e827f969f4c
Hakim Ghods <ghods.hakim@hotmail.com>
2020-02-10 04:47:41 +0330

    stram

--- StreamDemo/Hubs/StreamHub.cs
+++ StreamDemo/Hubs/StreamHub.cs
@@ -6,11 +6,21 @@ using System.Threading.Tasks;
 
 namespace StreamDemo.Hubs
 {
+    public class Coord
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
     public class StreamHub:Hub
     {
-        public async Task SendCoord(int x, int y)
+        public async  IAsyncEnumerable<Coord> SendCoord(IAsyncEnumerable<Coord> coords)
         {
-            await Clients.All.SendAsync("ReceiveCoord", x, y);
+            await foreach (var coord in coords)
+            {
+                await Clients.All.SendAsync("ReceiveCoord", coord.X, coord.Y);
+                yield return coord;
+            }
+            
         }
     }
 }
