29775225317c528198d5eade0396450362d17b48
Hakim Ghods <ghods.hakim@hotmail.com>
2020-02-15 11:12:48 +0330

    clean

--- StreamDemo/Hubs/StreamHub.cs
+++ StreamDemo/Hubs/StreamHub.cs
@@ -6,12 +6,6 @@ using System.Threading.Tasks;
 
 namespace StreamDemo.Hubs
 {
-    public class Coord
-    {
-        public int X { get; set; }
-        public int Y { get; set; }
-    }
-
     public class StreamHub : Hub
     {
         public async Task SendLine(IAsyncEnumerable<string> lines)
